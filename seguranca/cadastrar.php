37a268e8b3f3037cd68613a6e3b59bced4701c15
Eliseu Hairone <eliseuarviscoh@gmail.com>
2020-08-08 07:12:31 -0300

    Update cadastrar.php

--- seguranca/cadastrar.php
+++ seguranca/cadastrar.php
@@ -7,7 +7,7 @@ curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify")
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-    "secret" => "6LfRxrsZAAAAAGOkMm-XmFFKWzgwRsNMi8tUk5F7",
+    "secret" => "secret",
     "response" => $_POST["g-recaptcha-response"],
     "remoteip" => $_SERVER["REMOTE_ADDR"],
 )));
